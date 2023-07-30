4b18a9b79f20948d75e8ee78127a8889560320ec
mihirshinde06 <shindemihir0606@gmail.com>
2023-07-30 23:48:07 +0530

    Update OrderHistoryTableRow.tsx

--- src/pages/MyAccount/OrderHistory/OrderHistoryTableRow.tsx
+++ src/pages/MyAccount/OrderHistory/OrderHistoryTableRow.tsx
@@ -4,8 +4,6 @@ import { formatOrderId } from "../../../utils/utils";
 import CancelOrderConfirmationDialog from "./CancelOrderConfirmationDialog";
 import OrderDetailsDialog from "./OrderDetailsDialog";
 import { IOrderHistoryTableRowProps } from "./types";
-import Invoice from "./Invoice/Invoice";
-import { PDFDownloadLink } from "@react-pdf/renderer";
 
 const OrderHistoryTableRow = ({ orderDetails }: IOrderHistoryTableRowProps) => {
   const [openOrderDetails, setOpenOrderDetails] = useState(false);
